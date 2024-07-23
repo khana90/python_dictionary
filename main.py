438eedbda01cdba0eb3b9f85473214dab1c5f715
A khan <awal.khan@live.co.uk>
2024-07-23 00:26:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+word = input("Enter the word you want to translate: ")
+#print (f"found meaning for : {word}")
+
+wording_dictionary = {    "slingshot": "غولکه", 
+                          "exponent": "په طاقت د [عدد]",
+                           "spectrum": "پرخه ,طیف",
+                           "deprive": " محرومول" }
+
+print (f"translated: {wording_dictionary[word]}")
+
+
