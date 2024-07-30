adc6d853bdbcd8909785ed44aae341a26e0cb39a
A khan <awal.khan@live.co.uk>
2024-07-30 00:21:13 +0000

    updated
    added more words

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ word = input("Enter the word you want to translate: ")
 #print (f"found meaning for : {word}")
 
 wording_dictionary = {    "slingshot": "غولکه", 
-                          "exponent": "په طاقت د [عدد]",
+                           "exponent": "په طاقت د [عدد]",
                            "spectrum": "پرخه ,طیف",
-                           "deprive": " محرومول" }
+                           "deprive" : " محرومول" ,
+                            "quench" : "serab shodan" ,  
+                            "oblivious": "be khabar" ,
+                            "single out": " تعصب taasob "
+                     
+                     }
 
-print (f"translated: {wording_dictionary[word]}")
+print (f" The Answer is: {wording_dictionary[word]}")
 
 
